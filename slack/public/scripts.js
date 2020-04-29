e4702bb7aea94b096f356eb2ef4bf9f44250ff09
RevenkoSergei <23happyami@gmail.com>
2020-04-29 17:36:59 +0300

    upd namespace event listeners and endpoints

--- slack/public/scripts.js
+++ slack/public/scripts.js
@@ -1,8 +1,28 @@
-import io from '/socket.io/socket.io.js'
+// import io from '/socket.io/socket.io.js'
+// import io from 'socket.io/node_modules/socket.io-client';
 
 const socket = io('http://localhost:9000');
 
 socket.on('connect', () => {
-    console.log(socket.id)
-})
+  console.log(socket.id);
+});
 
+//todo listen from nsList, which is a list of all the namespaces
+socket.on('nsList', (nsData) => {
+  const namespacesDiv = document.querySelector('.namespaces');
+  namespacesDiv.innerHTML = '';
+  nsData.forEach((el) => {
+    namespacesDiv.innerHTML += `<div class='namespace' endpoint = ${el.endpoint}><img src="${el.img}"/></div>`;
+  });
+
+  //todo add a click listeners to each namespace
+  let namespaceDivs = document.querySelectorAll('.namespace');
+  namespaceDivs = Array.from(namespaceDivs);
+
+  namespaceDivs.map((nsDiv) => {
+    nsDiv.addEventListener('click', () => {
+        let endpoint = nsDiv.getAttribute('endpoint')
+        console.log(endpoint)
+    });
+  });
+});
--- slack/slack.js
+++ slack/slack.js
@@ -13,21 +13,26 @@ const expressServer = app.listen(9000, () => {
 const io = socketio(expressServer);
 
 io.on('connection', (socket) => {
-    socket.emit('messageFromServer', {data:"Welcome to the server"})
-    socket.on('messageFromClient',(msg) => {
-        console.log(msg)
-    })
-      let nsData = namespaces.map((ns) =>{
-      return {
-          img: ns.img,
-          endpoint: ns.endpoint
-      }
-  })
-    console.log(nsData)
+
+  // socket.emit('messageFromServer', { data: 'Welcome to the server' });
+  // socket.on('messageFromClient', (msg) => {
+  //   console.log(msg);
+  // });
+//todo We need to build an array to send back with the img and endpoint for each NS
+
+  let nsData = namespaces.map((ns) => {
+    return {
+      img: ns.img,
+      endpoint: ns.endpoint,
+    };
+  });
+    //todo We need to send ns data back to the client. We need to use socket, not io, bacause we want it to
+    //go to just this client
+    socket.emit('nsList', nsData)
 });
 
 namespaces.forEach((namespace) => {
-    io.of(namespace.endpoint).on('connection', (socket) => {
-        console.log(socket.id);
-    });
+  io.of(namespace.endpoint).on('connection', (socket) => {
+    console.log(socket.id);
+  });
 });
